b35da13136e564dcdd22af93833fb25228686211
Masahiro Yanou <yanou@atware.co.jp>
2019-12-10 14:36:43 +0900

    format

--- server/src/server.js
+++ server/src/server.js
@@ -4,25 +4,22 @@ const server = require('server');
 const { argv } = require('yargs');
 const open = require('open');
 
-const { send } = server.reply;
+const { error } = server.router;
+const { send, status } = server.reply;
 
 const port = argv.port || 8000;
-
-const index = (() => {
-  return readFileSync(path.join(__dirname, 'public', 'index.html'), {
-    encoding: 'UTF-8'
-  });
-})();
+const serverConfig = {
+  port,
+  security: false,
+  public: path.join(__dirname, 'public')
+};
 
 server(
-  {
-    port,
-    security: false,
-    public: path.join(__dirname, 'public')
-  },
+  serverConfig,
   require('./projects'),
   require('./datastore'),
-  () => send(index)
+  () => send((() => readFileSync(path.join(__dirname, 'public', 'index.html'), { encoding: 'UTF-8' }))()),
+  error(ctx => status(500).send(ctx.error.message))
 );
 
 if (!argv.skipBrowser) {
